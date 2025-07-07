1d430c19ed14fe215d7398d53e6f74ee317c5f2e
imayurchauhan <mjchauhan08@gmail.com>
2025-07-07 23:11:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -171,18 +171,18 @@ function printMarksheet() {
 
 
 
-// function downloadPDF() {
-//   const element = document.getElementById("marksheetContent");
-//   if (!element) return alert("Marksheet not found!");
+function downloadPDF() {
+  const element = document.getElementById("marksheetContent");
+  if (!element) return alert("Marksheet not found!");
 
-//   html2pdf().set({
-//     margin: 0.5,
-//     filename: "Marksheet.pdf",
-//     image: { type: "jpeg", quality: 0.98 },
-//     html2canvas: { scale: 2 },
-//     jsPDF: { unit: "in", format: "a4", orientation: "portrait" }
-//   }).from(element).save();
-// }
+  html2pdf().set({
+    margin: 0.5,
+    filename: "Marksheet.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "in", format: "a4", orientation: "portrait" }
+  }).from(element).save();
+}
 
 
 
